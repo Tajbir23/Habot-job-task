a498889276c7e2ab92f83817ded7f4e08d9604bb
Tajbir islam <studenttajbirislam@gmail.com>
2024-07-02 06:55:52 +0600

    first commit

---
+++ src/components/Navbar/DropDown.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+
+const DropDown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="relative inline-block">
+      <button
+        onClick={toggleDropdown}
+        className="relative z-10 block p-2 text-[#6D6E71]  border border-transparent rounded-md  focus:border-blue-500 focus:ring-opacity-40 dark:focus:ring-opacity-40 focus:ring-blue-300 focus:ring  focus:outline-none"
+      >
+        <div className='flex items-center justify-center gap-2'>
+        <span >Find service tags</span>
+        <svg
+          className="w-5 h-5 text-gray-800 "
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+            clipRule="evenodd"
+          />
+        </svg>
+        </div>
+      </button>
+
+      {isOpen && (
+        <div
+          className="absolute right-0 z-20 w-48 py-2 mt-2 origin-top-right bg-white rounded-md shadow-xl "
+          onClick={() => setIsOpen(false)}
+        >
+          <a
+            href="#"
+            className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform hover:bg-gray-100  "
+          >
+            Your profile
+          </a>
+          
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DropDown;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import DropDown from "./DropDown"
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center py-10">
+        <div>
+            <img className="h-11" src="/images/logo.svg" alt="logo" />
+        </div>
+        <div className="flex items-center gap-5 text-[#6D6E71]">
+            <a href="#" >Find Suppliers</a>
+            <div>
+            <DropDown />
+            </div>
+            <button className="border border-[#00732F] text-[#00732F] px-3 py-2 rounded-md focus:border-blue-500 focus:ring-opacity-40 dark:focus:ring-opacity-40 focus:ring-blue-300 focus:ring">Login/Sign Up</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
