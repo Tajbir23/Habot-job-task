9302cdfd87c37f011a706967db08cd44ac6d02d1
Tajbir islam <studenttajbirislam@gmail.com>
2024-07-02 07:01:59 +0600

    navbar done

--- src/components/Navbar/DropDown.jsx
+++ src/components/Navbar/DropDown.jsx
@@ -14,7 +14,7 @@ const DropDown = () => {
         className="relative z-10 block p-2 text-[#6D6E71]  border border-transparent rounded-md  focus:border-blue-500 focus:ring-opacity-40 dark:focus:ring-opacity-40 focus:ring-blue-300 focus:ring  focus:outline-none"
       >
         <div className='flex items-center justify-center gap-2'>
-        <span >Find service tags</span>
+        <span className='font-poppins font-normal' >Find service tags</span>
         <svg
           className="w-5 h-5 text-gray-800 "
           xmlns="http://www.w3.org/2000/svg"
@@ -32,7 +32,7 @@ const DropDown = () => {
 
       {isOpen && (
         <div
-          className="absolute right-0 z-20 w-48 py-2 mt-2 origin-top-right bg-white rounded-md shadow-xl "
+          className="absolute right-0 z-20 w-48 py-2 mt-2 origin-top-right bg-white rounded-md shadow-xl font-poppins font-normal"
           onClick={() => setIsOpen(false)}
         >
           <a
